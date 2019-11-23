e9cc7a7a2c0222c0a75d4ec7ec4b8a4a649de891
Abhay <abhay.singh@zyloto.com>
2019-11-23 21:29:41 +0530

    added react native paper

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import * as Font from 'expo-font'
 import React, { useState } from 'react'
 import { Platform, StatusBar, StyleSheet, View } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
+import { Provider as PaperProvider } from 'react-native-paper'
+import theme from './constants/Theme'
 
 import AppNavigator from './navigation/AppNavigator'
 
@@ -20,10 +22,12 @@ export default function App(props) {
   )
  } else {
   return (
-   <View style={styles.container}>
-    {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-    <AppNavigator />
-   </View>
+   <PaperProvider theme={theme}>
+    <View style={styles.container}>
+     {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+     <AppNavigator />
+    </View>
+   </PaperProvider>
   )
  }
 }
